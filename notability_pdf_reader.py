a6d49c40671b1303759e7df8932d2dcc594bde01
Dustin Wicker <31931512+dustinwicker@users.noreply.github.com>
2023-12-27 11:01:36 -0700

    Extract text and create DataFrame of results

--- notability_pdf_reader.py
+++ notability_pdf_reader.py
@@ -2,17 +2,11 @@ import os
 import pandas as pd
 from pdfminer.high_level import extract_text
 
-# Increase maximum width in characters of columns - will put all columns in same line in console readout
-pd.set_option("expand_frame_repr", False)
-# Increase number of rows printed out in console
-pd.set_option('display.max_rows', 600)
-# Able to read entire value in each column (no longer truncating values)
-pd.set_option('display.max_colwidth', None)
-
 # Change directory
 notability_directory = 'C:\\Users\\dustin.wicker\\PycharmProjects\\Notability'
 os.chdir(notability_directory)
 
+# Empty list to append results of extracted text to
 notability = []
 for i in os.listdir():
     if os.path.isfile(i):
@@ -31,8 +25,3 @@ for i in os.listdir():
 notability = pd.DataFrame(notability, columns=['file', 'folder', 'text'])
 notability.text = notability.text.str.lower()
 notability.to_pickle('notability_info.pkl')
-
-# start here
-notability = pd.read_pickle('notability_info.pkl')
-# Search notability
-notability.loc[notability.text.str.lower().str.contains('ridge regression'), ['file', 'folder']]
